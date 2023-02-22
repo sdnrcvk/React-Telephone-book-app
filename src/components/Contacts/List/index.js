8be405295a018871c773387102cacd99e1f744d8
Sedanur Çevik <sdnrcvk@gmail.com>
2023-02-23 00:58:01 +0300

    contacts filtered

--- src/components/Contacts/List/index.js
+++ src/components/Contacts/List/index.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function List({contacts}) {
+    const [filterText,setFilterText]=useState("");
+
+    const filtered=contacts.filter((item)=>{
+        return Object.keys(item).some((key)=>{
+            return item[key].toString().toLowerCase().includes(filterText.toLocaleLowerCase());
+        });
+    });
+
+    console.log(filtered);
+
   return (
     <div>
-        {contacts.map((contact,i)=>(
+        <input type="text" placeholder='Kişi Ara' value={filterText} onChange={(e)=>setFilterText(e.target.value)}/>
+        <ul>
+        {filtered.map((contact,i)=>(
             <li key={i}>{contact.fullname}-{contact.phone_number}</li>
         ))}
+        </ul>
     </div>
   )
 }
