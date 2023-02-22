fd9c9d358e6c5672d74aeeb9abe642f80bb59396
Sedanur Çevik <sdnrcvk@gmail.com>
2023-02-22 23:51:39 +0300

    form component edited

--- src/components/Contacts/Form/index.js
+++ src/components/Contacts/Form/index.js
@@ -1,7 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-function Form() {
-    const [form,setForm]=useState({fullname:"",phone_number:""});
+const initialValues={fullname:"",phone_number:""};
+function Form({addContact,contacts}) {
+    const [form,setForm]=useState(initialValues);
+
+    useEffect(()=>{
+        setForm(initialValues);
+    },[contacts])
 
     const onChangeInput=(e)=>{
         setForm({...form,[e.target.name]:e.target.value});
@@ -12,17 +17,17 @@ function Form() {
         if(form.fullname==="" || form.phone_number===""){
             return false;
         }
-        console.log(form);
+        addContact([...contacts,form]);
     };
 
   return (
 
     <form onSubmit={onSubmit}>
         <div>
-            <input type="text" name='fullname' placeholder='İsim Soyisim' onChange={onChangeInput}/>
+            <input type="text" name='fullname' placeholder='İsim Soyisim' value={form.fullname} onChange={onChangeInput}/>
         </div>
         <div>
-            <input type="text" name='phone_number' placeholder='Telefon Numarası' onChange={onChangeInput}/>
+            <input type="text" name='phone_number' placeholder='Telefon Numarası' value={form.phone_number} onChange={onChangeInput}/>
         </div>
         <div>
             <button>Ekle</button>
--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Form from './Form'
 import List from './List'
 
 function Contacts() {
+    const [contacts,setContacts]=useState([]);
+
+    useEffect(()=>{
+        console.log(contacts);
+
+    },[contacts])
+
   return (
     <div>
         <List/>
-        <Form/>
+        <Form addContact={setContacts} contacts={contacts}/>
     </div>
   )
 }
